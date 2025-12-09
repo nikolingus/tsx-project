a96899bb11c17a8ee5a2b09704075d40e51b95d5
Mikołaj <zhiltsov2281337@gmail.com>
2025-12-09 14:53:02 +0300

    video

--- src/Video/Video.tsx
+++ src/Video/Video.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Video.css";
 
+interface IVideoData {
+  src: string;
+}
+
 const Video: React.FC = () => {
+  const links: IVideoData[] = React.useMemo(
+    () => [
+      {
+        src: "https://rutube.ru/play/embed/d1a9dac79cb0525f062d29f16e5f3dc0?t=1671",
+      },
+    ],
+    []
+  );
   return (
     <section className="video section">
-      <iframe
-        className="video__frame"
-        src="https://rutube.ru/play/embed/d1a9dac79cb0525f062d29f16e5f3dc0?t=1671"
-      ></iframe>
+      {links.map((link, index) => (
+        <iframe className="video__frame" src={link.src} key={index}></iframe>
+      ))}
     </section>
   );
 };
