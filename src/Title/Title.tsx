31e64635feb30bd75c1fbf179b6bbff8b39dbe78
Mikołaj <zhiltsov2281337@gmail.com>
2025-12-10 13:00:36 +0300

    t

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./Title.css";
 
 const Title: React.FC = () => {
-  const subtext: string =
-    "УВЛЕКАТЕЛЬНЫЕ ТУРЫ ПО КИТАЮ С ОСМОТРОМ ДОСТОПРИМЕЧАТЕЛЬНОСТЕЙ";
-  const text: string =
-    "Насыщенные экскурсионные туры в Китай, интересная архитектура и богатая история.";
-
   return (
     <section className="title section" id="about">
-      <h6 className="title__subtext">{subtext}</h6>
-      <h1 className="title__text">{text}</h1>
+      <h6 className="title__subtext">
+        УВЛЕКАТЕЛЬНЫЕ ТУРЫ ПО КИТАЮ С ОСМОТРОМ ДОСТОПРИМЕЧАТЕЛЬНОСТЕЙ
+      </h6>
+      <h1 className="title__text">
+        Насыщенные экскурсионные туры в Китай, интересная архитектура и богатая
+        история.
+      </h1>
     </section>
   );
 };
