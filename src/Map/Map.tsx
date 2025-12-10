afa0eebede06c1b34b9eac3b81e8e1a3a1de9592
Mikołaj <zhiltsov2281337@gmail.com>
2025-12-10 12:56:17 +0300

    v

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Map.css";
 
 const Map: React.FC = () => {
-  const mapLink: string =
-    "https://yandex.ru/map-widget/v1/?ll=30.326622%2C59.955079&mode=whatshere&whatshere%5Bpoint%5D=30.326788%2C59.955161&whatshere%5Bzoom%5D=17&z=17.39";
   return (
     <section className="map section" id="map">
-      <iframe className="map__frame" src={mapLink} loading="lazy"></iframe>
+      <iframe
+        className="map__frame"
+        src="https://yandex.ru/map-widget/v1/?ll=30.326622%2C59.955079&mode=whatshere&whatshere%5Bpoint%5D=30.326788%2C59.955161&whatshere%5Bzoom%5D=17&z=17.39"
+        loading="lazy"
+      ></iframe>
     </section>
   );
 };
--- src/Video/Video.tsx
+++ src/Video/Video.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./Video.css";
 
 const Video: React.FC = () => {
-  const link: string = React.useMemo(
-    () =>
-      "https://rutube.ru/play/embed/d1a9dac79cb0525f062d29f16e5f3dc0?t=1671",
-    []
-  );
   return (
     <section className="video section">
-      <iframe className="video__frame" src={link}></iframe>
+      <iframe
+        className="video__frame"
+        src="https://rutube.ru/play/embed/d1a9dac79cb0525f062d29f16e5f3dc0?t=1671"
+      ></iframe>
     </section>
   );
 };
